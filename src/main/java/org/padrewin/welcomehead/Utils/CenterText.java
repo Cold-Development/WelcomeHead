999d294ca0a4c26f10085e2760660a834fbf5f70
padrewin <97427230+padrewin@users.noreply.github.com>
2025-01-07 17:17:19 +0200

    Update CenterText.java

--- src/main/java/org/padrewin/welcomehead/Utils/CenterText.java
+++ src/main/java/org/padrewin/welcomehead/Utils/CenterText.java
@@ -3,44 +3,52 @@ package org.padrewin.welcomehead.Utils;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.entity.Player;
 
-public class CenterText
-{
+public class CenterText {
   private static final int CENTER_PX = 154;
 
   public static void sendCenteredMessage(Player player, String message) {
-if (message == null || message.equals("")) player.sendMessage("");
-message = PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", message));
+    if (message == null || message.equals("")) {
+      player.sendMessage("");
+      return;
+    }
 
-int messagePxSize = 0;
-boolean previousCode = false;
-boolean isBold = false;
+    // Aplicăm PlaceholderAPI și traducem culorile
+    message = PlaceholderAPI.setPlaceholders(player, Utils.translateColors(message));
 
-for (char c : message.toCharArray()) {
-  if (c == '§')
-  { previousCode = true; }
+    int messagePxSize = 0;
+    boolean previousCode = false;
+    boolean isBold = false;
 
-  else if (previousCode == true)
-  { previousCode = false;
-    if (c == 'l' || c == 'L') {
-      isBold = true;
+    // Calculăm dimensiunea pixelilor mesajului
+    for (char c : message.toCharArray()) {
+      if (c == '§') {
+        previousCode = true;
+      } else if (previousCode) {
+        previousCode = false;
+        if (c == 'l' || c == 'L') {
+          isBold = true;
         } else {
-      isBold = false;
-        }  }
-  else { InfoEnum dFI = InfoEnum.getDefaultFontInfo(c);
-    messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
-    messagePxSize++; }
-
+          isBold = false;
+        }
+      } else {
+        InfoEnum dFI = InfoEnum.getDefaultFontInfo(c);
+        messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+        messagePxSize++;
+      }
     }
 
-int halvedMessageSize = messagePxSize / 2;
-int toCompensate = 154 - halvedMessageSize;
-int spaceLength = InfoEnum.SPACE.getLength() + 1;
-int compensated = 0;
-StringBuilder sb = new StringBuilder();
-while (compensated < toCompensate) {
-  sb.append(" ");
-  compensated += spaceLength;
+    // Calculăm spațiile necesare pentru centrare
+    int halvedMessageSize = messagePxSize / 2;
+    int toCompensate = CENTER_PX - halvedMessageSize;
+    int spaceLength = InfoEnum.SPACE.getLength() + 1;
+    int compensated = 0;
+    StringBuilder sb = new StringBuilder();
+    while (compensated < toCompensate) {
+      sb.append(" ");
+      compensated += spaceLength;
     }
-player.sendMessage(sb.toString() + message);
+
+    // Trimitem mesajul centrat
+    player.sendMessage(sb.toString() + message);
   }
 }
