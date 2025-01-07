5e10ab259b45fd8441a9d695094d0df8123eaf37
padrewin <97427230+padrewin@users.noreply.github.com>
2025-01-07 17:52:26 +0200

    Update Commands.java

--- src/main/java/org/padrewin/welcomehead/commands/Commands.java
+++ src/main/java/org/padrewin/welcomehead/commands/Commands.java
@@ -12,11 +12,10 @@ public class Commands implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         String prefix = WelcomeHead.prefix;
 
-        // Verificăm dacă sender-ul este un jucător
+        // Dacă comanda este executată de un jucător
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
-            // Verificăm permisiunile
             if (player.isOp() || player.hasPermission("welcomehead.admin")) {
                 if (args.length == 0) {
                     player.sendMessage(Utils.translateColors(prefix + "Developed by padrewin!"));
@@ -24,27 +23,39 @@ public class Commands implements CommandExecutor {
                 }
 
                 if (args[0].equalsIgnoreCase("reload")) {
-                    // Reîncărcăm configurația plugin-ului
                     WelcomeHead.getInstance().reloadConfig();
                     WelcomeHead.getInstance().saveDefaultConfig();
                     WelcomeHead.getInstance().reloadConfig();
 
-                    // Actualizăm prefixul folosind metoda centralizată de traducere a culorilor
                     prefix = Utils.translateColors(WelcomeHead.getInstance().getConfig().getString("prefix"));
                     WelcomeHead.prefix = prefix;
 
-                    // Trimitem mesajul de confirmare cu suport pentru culori
-                    sender.sendMessage(Utils.translateColors(prefix + "Reload completed!"));
+                    player.sendMessage(Utils.translateColors(prefix + "Reload completed!"));
                     return true;
                 }
             }
+        } else {
+            // Dacă comanda este executată din consolă
+            if (args.length == 0) {
+                sender.sendMessage(Utils.translateColors(prefix + "Developed by padrewin!"));
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("reload")) {
+                WelcomeHead.getInstance().reloadConfig();
+                WelcomeHead.getInstance().saveDefaultConfig();
+                WelcomeHead.getInstance().reloadConfig();
+
+                prefix = Utils.translateColors(WelcomeHead.getInstance().getConfig().getString("prefix"));
+                WelcomeHead.prefix = prefix;
+
+                sender.sendMessage(Utils.translateColors(prefix + "Reload completed from console!"));
+                return true;
+            }
         }
 
-        // Dacă sender-ul nu este un jucător sau nu are permisiuni
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(Utils.translateColors(prefix + "This command can only be run by a player!"));
-        }
-
+        sender.sendMessage(Utils.translateColors(prefix + "You do not have permission to run this command!"));
         return false;
     }
+
 }
