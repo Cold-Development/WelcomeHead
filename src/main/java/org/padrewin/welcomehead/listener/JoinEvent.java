db98d0cfe2b49c0d4042cec27dc49f9fbe600732
padrewin <97427230+padrewin@users.noreply.github.com>
2025-01-07 17:17:17 +0200

    Update JoinEvent.java

--- src/main/java/org/padrewin/welcomehead/listener/JoinEvent.java
+++ src/main/java/org/padrewin/welcomehead/listener/JoinEvent.java
@@ -5,10 +5,12 @@ import org.padrewin.welcomehead.Utils.ImageChar;
 import org.padrewin.welcomehead.Utils.ImageMessage;
 import org.padrewin.welcomehead.Utils.Utils;
 import org.padrewin.welcomehead.WelcomeHead;
+
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 import javax.imageio.ImageIO;
+
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -21,93 +23,84 @@ public class JoinEvent implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(final PlayerJoinEvent e) throws IOException {
-        final Player player = e.getPlayer(); // Capture 'e' in a final variable
-        if (!player.hasPlayedBefore()) {
-            final int spaceTop = WelcomeHead.getInstance().getConfig().getInt("Spaces-Top");
-            final int spacesBot = WelcomeHead.getInstance().getConfig().getInt("Spaces-Bot");
-            Bukkit.getScheduler().scheduleSyncDelayedTask((Plugin)WelcomeHead.getInstance(), new Runnable() {
-                public void run() {
-                    if (WelcomeHead.getInstance().getConfig().getBoolean("Players-FirstJoin.enable")) {
-                        for (int i = 0; i <= spaceTop; i++)
-                            player.sendMessage(" ");
-                        BufferedImage imageToSend = null;
-                        try {
-                            imageToSend = ImageIO.read(new URL("https://minotar.net/avatar/" + player.getName() + "/8.png"));
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                        (new ImageMessage(imageToSend, 8, ImageChar.BLOCK.getChar()))
-                                .appendText(new String[] {
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-FirstJoin.head-text.1"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-FirstJoin.head-text.2"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-FirstJoin.head-text.3"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-FirstJoin.head-text.4"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-FirstJoin.head-text.5"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-FirstJoin.head-text.6"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-FirstJoin.head-text.7"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-FirstJoin.head-text.8"))) })
-                                .sendToPlayer(player);
-                        for (int j = 0; j <= spacesBot; j++)
-                            player.sendMessage(" ");
-                    } else {
-                        Utils.headNotActivated(player, WelcomeHead.getInstance().getConfig().getStringList("Players-FirstJoin.no-head-text"),
-                                WelcomeHead.getInstance().getConfig().getBoolean("Players-FirstJoin.center"));
-                    }
-                    Utils.soundActivated(player, WelcomeHead.getInstance().getConfig().getBoolean("SoundA.enable"),
-                            WelcomeHead.getInstance().getConfig().getString("SoundA.sound"),
-                            WelcomeHead.getInstance().getConfig().getDouble("SoundA.volume"),
-                            WelcomeHead.getInstance().getConfig().getDouble("SoundA.pitch"));
-                    Utils.spawnFireworks(player.getLocation(), WelcomeHead.getInstance().getConfig().getInt("Firework.amount"));
-                    Utils.commandsActivated(player, WelcomeHead.getInstance().getConfig().getStringList("Commands-First"), "Something is weird with your command -> First player");
+        final Player player = e.getPlayer();
+        final int spaceTop = WelcomeHead.getInstance().getConfig().getInt("Spaces-Top");
+        final int spacesBot = WelcomeHead.getInstance().getConfig().getInt("Spaces-Bot");
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask((Plugin) WelcomeHead.getInstance(), new Runnable() {
+            public void run() {
+                if (!player.hasPlayedBefore()) {
+                    handleFirstJoin(player, spaceTop, spacesBot);
+                } else {
+                    handleBackJoin(player, spaceTop, spacesBot);
                 }
-            }, WelcomeHead.getInstance().getConfig().getInt("Timer") * 20L);
+            }
+        }, WelcomeHead.getInstance().getConfig().getInt("Timer") * 20L);
+    }
+
+    private void handleFirstJoin(Player player, int spaceTop, int spacesBot) {
+        if (WelcomeHead.getInstance().getConfig().getBoolean("Players-FirstJoin.enable")) {
+            sendEmptySpaces(player, spaceTop);
+            sendImageMessage(player, "Players-FirstJoin.head-text");
+            sendEmptySpaces(player, spacesBot);
         } else {
-            final int spaceTop = WelcomeHead.getInstance().getConfig().getInt("Spaces-Top");
-            final int spacesBot = WelcomeHead.getInstance().getConfig().getInt("Spaces-Bot");
-            Bukkit.getScheduler().scheduleSyncDelayedTask((Plugin)WelcomeHead.getInstance(), new Runnable() {
-                public void run() {
-                    if (WelcomeHead.getInstance().getConfig().getBoolean("Players-Back.enable")) {
-                        for (int i = 0; i <= spaceTop; i++)
-                            player.sendMessage(" ");
-                        BufferedImage imageToSend = null;
-                        try {
-                            imageToSend = ImageIO.read(new URL("https://minotar.net/avatar/" + player.getName() + "/8.png"));
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                        (new ImageMessage(imageToSend, 8, ImageChar.BLOCK.getChar()))
-                                .appendText(new String[] {
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-Back.head-text.1"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-Back.head-text.2"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-Back.head-text.3"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-Back.head-text.4"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-Back.head-text.5"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-Back.head-text.6"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-Back.head-text.7"))),
-                                        PlaceholderAPI.setPlaceholders(player, Utils.translateHexColorCodes("&#", "", WelcomeHead.getInstance().getConfig().getString("Players-Back.head-text.8"))) })
-                                .sendToPlayer(player);
-                        for (int j = 0; j <= spacesBot; j++)
-                            player.sendMessage(" ");
-                        Utils.soundActivated(player, WelcomeHead.getInstance().getConfig().getBoolean("SoundB.enable"),
-                                WelcomeHead.getInstance().getConfig().getString("SoundB.sound"),
-                                WelcomeHead.getInstance().getConfig().getDouble("SoundB.volume"),
-                                WelcomeHead.getInstance().getConfig().getDouble("SoundB.pitch"));
-                        Utils.commandsActivated(player, WelcomeHead.getInstance().getConfig().getStringList("Commands-Back"), "Something is weird with your command -> Back player");
-                    } else {
-                        for (int i = 0; i <= spacesBot; i++)
-                            player.sendMessage(" ");
-                        Utils.headNotActivated(player, WelcomeHead.getInstance().getConfig().getStringList("Players-Back.no-head-text"),
-                                WelcomeHead.getInstance().getConfig().getBoolean("Players-Back.center"));
-                        Utils.soundActivated(player, WelcomeHead.getInstance().getConfig().getBoolean("SoundB.enable"),
-                                WelcomeHead.getInstance().getConfig().getString("SoundB.sound"),
-                                WelcomeHead.getInstance().getConfig().getDouble("SoundB.volume"),
-                                WelcomeHead.getInstance().getConfig().getDouble("SoundB.pitch"));
-                        Utils.commandsActivated(player, WelcomeHead.getInstance().getConfig().getStringList("Commands-Back"), "Something is weird with your command -> Back player");
-                        for (int i = 0; i <= spacesBot; i++)
-                            player.sendMessage(" ");
-                    }
-                }
-            }, WelcomeHead.getInstance().getConfig().getInt("Timer") * 20L);
+            Utils.headNotActivated(player,
+                    WelcomeHead.getInstance().getConfig().getStringList("Players-FirstJoin.no-head-text"),
+                    WelcomeHead.getInstance().getConfig().getBoolean("Players-FirstJoin.center"));
         }
+
+        playEffectsAndCommands(player, "SoundA", "Commands-First", "First player");
+    }
+
+    private void handleBackJoin(Player player, int spaceTop, int spacesBot) {
+        if (WelcomeHead.getInstance().getConfig().getBoolean("Players-Back.enable")) {
+            sendEmptySpaces(player, spaceTop);
+            sendImageMessage(player, "Players-Back.head-text");
+            sendEmptySpaces(player, spacesBot);
+        } else {
+            Utils.headNotActivated(player,
+                    WelcomeHead.getInstance().getConfig().getStringList("Players-Back.no-head-text"),
+                    WelcomeHead.getInstance().getConfig().getBoolean("Players-Back.center"));
+        }
+
+        playEffectsAndCommands(player, "SoundB", "Commands-Back", "Back player");
+    }
+
+    private void sendEmptySpaces(Player player, int spaces) {
+        for (int i = 0; i <= spaces; i++) {
+            player.sendMessage(" ");
+        }
+    }
+
+    private void sendImageMessage(Player player, String configPath) {
+        try {
+            BufferedImage imageToSend = ImageIO.read(new URL("https://minotar.net/avatar/" + player.getName() + "/8.png"));
+            new ImageMessage(imageToSend, 8, ImageChar.BLOCK.getChar())
+                    .appendText(getColoredText(player, configPath))
+                    .sendToPlayer(player);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private String[] getColoredText(Player player, String configPath) {
+        String[] textLines = new String[8];
+        for (int i = 1; i <= 8; i++) {
+            String line = WelcomeHead.getInstance().getConfig().getString(configPath + "." + i);
+            textLines[i - 1] = PlaceholderAPI.setPlaceholders(player, Utils.translateColors(line));
+        }
+        return textLines;
+    }
+
+    private void playEffectsAndCommands(Player player, String soundConfig, String commandsConfig, String errorMsg) {
+        Utils.soundActivated(player,
+                WelcomeHead.getInstance().getConfig().getBoolean(soundConfig + ".enable"),
+                WelcomeHead.getInstance().getConfig().getString(soundConfig + ".sound"),
+                WelcomeHead.getInstance().getConfig().getDouble(soundConfig + ".volume"),
+                WelcomeHead.getInstance().getConfig().getDouble(soundConfig + ".pitch"));
+        Utils.spawnFireworks(player.getLocation(), WelcomeHead.getInstance().getConfig().getInt("Firework.amount"));
+        Utils.commandsActivated(player,
+                WelcomeHead.getInstance().getConfig().getStringList(commandsConfig),
+                "Something is weird with your command -> " + errorMsg);
     }
 }
